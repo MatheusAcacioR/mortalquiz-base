9cf8833e5ded9ddd3401086690d70f9d1777b557
Matheus Acácio <matheusacacio@unigranrio.br>
2021-01-25 20:44:49 -0300

    codigo ao fina lda primeira aula imersao react

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,55 @@
+import { delBasePath } from 'next/dist/next-server/lib/router/router'
 import styled from 'styled-components'
+import db from '../db.json'
+import Widget from '../src/components/Widget'
+import Footer from '../src/components/Footer'
+import QuizBackground from '../src/components/QuizBackground'
+import GitHubCorner from '../src/components/GitHubCorner'
 
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
+// const BackgroundImage = styled.div`
+//   background-image: url(${db.bg});
+//   flex: 1;
+//   background-size: cover;
+//   background-position: center
+// `
+
+export const QuizContainer = styled.div`
+  width: 100%;
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 10%;
+  @media screen and (max-width: 500px) {
+    margin: auto;
+    padding: 15px;
+  }
 `
 
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+    <QuizBackground backgroundImage={db.bg}>
+    <QuizContainer>
+      <Widget>
+          <Widget.Header>
+            <h1>Mortal Kombat</h1>
+
+          </Widget.Header>
+        <Widget.Content>
+
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex distinctio nulla, voluptates deserunt sequi modi maxime porro reprehenderit, nam quidem possimus odio reiciendis optio et autem numquam tempora, molestias a.</p>
+        </Widget.Content>
+      </Widget>
+        
+      <Widget>
+        <Widget.Content>
+
+        <h1>Quiz da galera</h1>
+
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ex distinctio nulla, voluptates deserunt sequi modi maxime porro reprehenderit, nam quidem possimus odio reiciendis optio et autem numquam tempora, molestias a.</p>
+        </Widget.Content>
+      </Widget>
+      <Footer />
+    </QuizContainer>
+    <GitHubCorner projectUrl="https://github.com/matheus-2000"/>
+    </QuizBackground>
+  )
 }
