1bd471f81f67c889f72a75ec1dcf41d8ec9a7c90
Matheus Acácio <matheusacacio@unigranrio.br>
2021-02-01 02:20:35 -0300

    projeto finalizado

--- pages/quiz/[id].js
+++ pages/quiz/[id].js
@@ -1,10 +1,48 @@
 /* eslint-disable linebreak-style */
+/* eslint-disable react/prop-types */
+/* eslint-disable linebreak-style */
 import React from 'react';
+import { ThemeProvider } from 'styled-components';
+import QuizScreen from '../../src/screens/Quiz';
 
-export default function QuizDaGaleraPage() {
+export default function QuizDaGaleraPage({ dbExterno }) {
   return (
-    <div>
-      Desafio da proxima aula junto com as animações
-    </div>
+    <ThemeProvider theme={dbExterno.theme}>
+      <QuizScreen externalQuestions={dbExterno.questions} externalBg={dbExterno.bg} />
+
+    </ThemeProvider>
+
+  // {/* <pre style={{ color: 'black' }}>
+  //   {JSON.stringify(dbExterno.questions, null, 4)}
+  // </pre> */}
   );
+}
+
+export async function getServerSideProps(context) {
+  const [projectName, gitHubUser] = context.query.id.split('___');
+  try {
+    const dbExterno = await fetch(`https://${projectName}.${gitHubUser}.vercel.app/api/db`)
+      .then((respostaDoServer) => {
+        if (respostaDoServer.ok) {
+          return respostaDoServer.json();
+        }
+
+        throw new Error('Falha ao buscar os dados');
+      })
+      .then((respostaConvertidaEmObjeto) => respostaConvertidaEmObjeto);
+      // .catch((err) => {
+      //   console.error(err);
+      // });
+
+    // console.log(dbExterno);
+    // console.log(context.query.id);
+
+    return {
+      props: {
+        dbExterno,
+      },
+    };
+  } catch (err) {
+    throw new Error(err);
+  }
 }
